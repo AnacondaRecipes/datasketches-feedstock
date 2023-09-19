0864feaaa226e646f02537c5c8335dfa7914c997
Serhii Kupriienko <skupriienko@anaconda.com>
2023-09-19 18:15:39 +0300

    Debug on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,8 +4,6 @@ cd build
 cmake -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -D CMAKE_BUILD_TYPE=Release ^
-    -D HDF5_INCLUDE_DIR=%LIBRARY_PREFIX%\include ^
-    -D HDF5_LIB_PATH=%LIBRARY_PREFIX%\lib ^
     -G "NMake Makefiles" ..
 if errorlevel 1 exit 1
 
