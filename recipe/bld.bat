61d9b4c2c4c1fcfdcf1d8aa5f40b6c4f4497321e
Serhii Kupriienko <skupriienko@anaconda.com>
2023-09-19 18:15:19 +0300

    Debug on win

---
+++ recipe/bld.bat
@@ -0,0 +1,21 @@
+mkdir build
+cd build
+
+cmake -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+    -D CMAKE_BUILD_TYPE=Release ^
+    -D HDF5_INCLUDE_DIR=%LIBRARY_PREFIX%\include ^
+    -D HDF5_LIB_PATH=%LIBRARY_PREFIX%\lib ^
+    -G "NMake Makefiles" ..
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
+
+nmake test
+if errorlevel 1 exit 1
+
+%PYTHON% -m pip install . --no-deps --no-build-isolation -vv
